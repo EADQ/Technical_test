d043d9e7c32370bc7b89062652ad6647ab6bb0ac
EADQ <easoftwareengineer@gmail.com>
2023-05-19 20:16:43 -0600

    second exercise

---
+++ palindromo.py
@@ -0,0 +1,16 @@
+Ejercicio:
+Escribe una función en Python llamada "es_palindromo" que reciba una cadena de texto y determine si es un palíndromo. Un palíndromo es una palabra, frase o secuencia de caracteres que se lee igual de izquierda a derecha y viceversa, sin tener en cuenta los espacios ni la puntuación.
+
+Tips y pistas:
+- Puedes utilizar métodos de cadenas como `lower()` para convertir la cadena a minúsculas y `replace()` para eliminar los espacios y la puntuación.
+- Puedes utilizar un bucle `for` o rebanado de cadenas para comparar los caracteres de la cadena original con su reverso.
+- Recuerda que si la cadena es un palíndromo, la comparación debería ser verdadera para todos los caracteres.
+- Al final de la función, debes retornar `True` si es un palíndromo y `False` si no lo es.
+
+
+Lista de Palabras que son palindromos: sometemos, sacas, rotor, aviva
+
+def es_palindromo(texto):
+
+
+
