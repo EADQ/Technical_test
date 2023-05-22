530b29f40bf48c37cabe02b80f57417be946edf8
EADQ <easoftwareengineer@gmail.com>
2023-05-21 21:44:23 -0600

    finish the second exersice

--- palindromo.py
+++ palindromo.py
@@ -1,16 +1,21 @@
-Ejercicio:
-Escribe una función en Python llamada "es_palindromo" que reciba una cadena de texto y determine si es un palíndromo. Un palíndromo es una palabra, frase o secuencia de caracteres que se lee igual de izquierda a derecha y viceversa, sin tener en cuenta los espacios ni la puntuación.
+# Ejercicio:
+# Escribe una función en Python llamada "es_palindromo" que reciba una cadena de texto y determine si es un palíndromo. Un palíndromo es una palabra, frase o secuencia de caracteres que se lee igual de izquierda a derecha y viceversa, sin tener en cuenta los espacios ni la puntuación.
 
-Tips y pistas:
-- Puedes utilizar métodos de cadenas como `lower()` para convertir la cadena a minúsculas y `replace()` para eliminar los espacios y la puntuación.
-- Puedes utilizar un bucle `for` o rebanado de cadenas para comparar los caracteres de la cadena original con su reverso.
-- Recuerda que si la cadena es un palíndromo, la comparación debería ser verdadera para todos los caracteres.
-- Al final de la función, debes retornar `True` si es un palíndromo y `False` si no lo es.
-
-
-Lista de Palabras que son palindromos: sometemos, sacas, rotor, aviva
-
-def es_palindromo(texto):
+# Tips y pistas:
+# - Puedes utilizar métodos de cadenas como `lower()` para convertir la cadena a minúsculas y `replace()` para eliminar los espacios y la puntuación.
+# - Puedes utilizar un bucle `for` o rebanado de cadenas para comparar los caracteres de la cadena original con su reverso.
+# - Recuerda que si la cadena es un palíndromo, la comparación debería ser verdadera para todos los caracteres.
+# - Al final de la función, debes retornar `True` si es un palíndromo y `False` si no lo es.
 
 
+# Lista de Palabras que son palindromos: sometemos, sacas, rotor, aviva
 
+def es_palindromo(text):
+    text_palindromo = text[::-1]
+    working_text = text_palindromo.lower().replace(" ", "").replace(".", "").replace(",", "")
+    if working_text == text:
+        print('La palabra', text,  'es un palíndromo')
+    else:
+        print('La palabra: ', text, 'no es un palíndromo, intenta con otra palabra')
+
+es_palindromo('sacas')
