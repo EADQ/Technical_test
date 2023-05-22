69f4f4d95e4727925fc3cc0049099a0dd54cb1a0
EADQ <easoftwareengineer@gmail.com>
2023-05-21 21:47:35 -0600

    new challenge

---
+++ conteo_vocales.py
@@ -0,0 +1,10 @@
+Ejercicio:
+Escribe una función en Python llamada "conteo_vocales" que reciba una cadena de texto y devuelva un diccionario con el conteo de cada vocal presente en la cadena. El diccionario debe tener las vocales como claves y el número de veces que aparecen como valores.
+
+Tips y pistas:
+- Puedes utilizar un bucle `for` para recorrer cada caracter de la cadena.
+- Puedes utilizar el método `lower()` para convertir todos los caracteres a minúsculas y así evitar diferencias de mayúsculas y minúsculas.
+- Puedes utilizar un condicional `if` para verificar si cada caracter es una vocal.
+- Utiliza un diccionario para ir almacenando el conteo de cada vocal.
+- Al final de la función, debes retornar el diccionario con los conteos.
+
